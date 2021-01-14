0ed7d61e1a2079fa765a3584568a622f9f3e48c2
Vito Gentile <gentilevito1076@gmail.com>
2021-01-14 09:16:58 +0100

    Aggiornamento: Codice Provato ma non funzionante

--- main.js
+++ main.js
@@ -1,53 +1,96 @@
-//
-// $(document).ready(function() {
-//
-//   $('.button-next').click(function() {
-//     $('.slider-box.active').removeClass('active').next().addClass('active');
-//   })
-//
-//   $('.button-prev').click(function() {
-//     $('.slider-box.active').removeClass('active').prev().addClass('active');
-//   })
-//
-// })
-
-// function reFunction() {
-//
-// }
-
-
-// function reFunction(parametro) {
-//   console.log('La foto in oggetto è: ' + parametro);
-// }
-
 $(document).ready(function() {
+  // function counter() {
+  //   for (var i = 0; i < $('img').length; i ++) {
+  //     if ($('img')[i] === $('img.last')) {
+  //       console.log($('img')[i]);
+  //     }
+  //   }
+  // }
+  // // ---------------Prove Es. Funzioni--------------------------------------
+  // // FX Funzionante---------------------------------------------------------
+  // function somma(a,b) {
+  //   var c = a + b;
+  //   return c;
+  // }
+  // var elSomma = somma(24,26);
+  // console.log(elSomma);
+  //
+  // //Porzione di Codice Funzionante------------------------------------------
+  // function somma(a,b) {
+  //   var c = a + b;
+  //   return c;
+  // }
+  //
+  // $('.next').click(function() {
+  //   var elSomma = somma(24,26);
+  //   console.log(elSomma);
+  // });
+  //
+  // //Anche questa porzione funziona------------------------------------------
+  // function fxSlider(imgQualsiasi) {
+  //   $(imgQualsiasi).removeClass('active').next().addClass('active');
+  // }
+  // $('.next').click(function() {
+  //   fxSlider('.images img.active');
+  //   console.log('questo codice risponde');
+  //   // console.log(elImg);
+  // });
+  // // -----------------------------------------------------------------------
+  // // -----------------------------------------------------------------------
 
-  function counter() {
-    for (var i = 0; i < $('img').length; i ++) {
-      if ($('img')[i] === $('img.last')) {
-        console.log($('img')[i]);
-      }
-    }
+  // // ULTIMO CODICE PROVATO--------------------------------------------------
+  function fxSlider(imgQualsiasi) {
+    console.log($(imgQualsiasi));
+    $(imgQualsiasi).removeClass('active').next().addClass('active');
+    console.log('questo codice risponde');
+    console.log($(imgQualsiasi));//*
+    return $(imgQualsiasi);
   }
-  var imgEl = $('img.last');
-  console.log('Questa è l\'ultima immagine: ');
-  console.log(imgEl);
-
   $('.next').click(function() {
-    counter();
-    $('.images img.active').removeClass('active').next().addClass('active');
+    var imgNext = fxSlider('.images img.active');
+    // if ($('.images img.active').class !== $('.images img.last').class) {
+    // if ($('.images img.active').attr('class') !== $('.images img.last').attr('class')) {
+    if (imgNext !== $('img.last.active')) {
+      fxSlider('imgNext');
+      console.log(imgNext);//*
+    } else {
+      fxSlider('.images img.active.first');
+    }
   });
+  // -------------------------------------------------------------------------------------
+  // -------------------------------------------------------------------------------------
+  // -------------------------------------------------------------------------------------
+  // -----questa va benissimo per mandare avanti le immagini (fino ad un certo punto)-----
 
+  // $('.next').click(function() {
+  //   $('.images img.active').removeClass('active').next().addClass('active');
+  //   console.log('OK NEL CLICK');
+  // });
+
+  // -----questa va benissimo per mandare indietro le immagini (fino ad un certo punto)----
 
   $('.prev').click(function() {
     $('.images img.active').removeClass('active').prev().addClass('active');
-
   });
-
+  // -------------------------------------------------------------------------------------
+  // -------------------------------------------------------------------------------------
+  // -------------------------------------------------------------------------------------
+  // ----------------Prove Console log OK-------------------------------------------------
+  $('div:first-child').attr('class');
+  console.log($('div:first-child').attr('class'));
+  $('img:first-child').attr('class');
+  console.log($('img:first-child').attr('class'));
+  // $('img:nth-child(2)').attr('class');
+  // console.log($('img:nth-child(2)').attr('class'));
+  var elClasse = $('img')[3];
+  console.log(elClasse.className);
   console.log('Questa è la lunghezza del mio array di immagini: ' + $('img').length);
   console.log('Questa è l\'ultima immagine: ');
   console.log($('img')[3]);
   console.log('Questa è l\'array di immagini: ');
   console.log($('img'));
+  // console.log($('img.last'));
 
+  console.log('PROVA FUORI DAL CLICK');
 });
+// --------------------------------------------------------------------------------------
