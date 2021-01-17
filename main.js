04d432ccd052a4b0525d8f11077b3104f2c6934d
Vito Gentile <gentilevito1076@gmail.com>
2021-01-17 20:33:51 +0100

    Commenti

--- main.js
+++ main.js
@@ -3,8 +3,11 @@ $(document).ready(function() { //Esegui le istruzioni di codice che seguono solo
   $('.next').click(function() { //Al click sull'elemento di Classe 'next' verrà eseguito il blocco di codice tra Graffe
     if ( $('.last').hasClass('active')) { //Se la Classe 'last' ha Classe 'active' (ed è quindi visibile, perchè dotata di display inline-block), esegui il codice tra Graffe.
       $('.last').removeClass('active'); // Rimuove la Classe 'active' restituendo alla classe' last' il suo display none (che, come da CSS, è associato a tutte le img)
-      $('.first').addClass('active'); // Ripristina la classe 'active' alla prima immagine
+      $('.first').addClass('active'); // Ripristina la classe 'active' alla prima immagine, consentendomi di ripartire daccapo
+    } else {
+      $ ('.active').removeClass('active').next().addClass('active'); // Altrimenti rimuovi la Classe 'active' dall'immagine corrente (per nasconderla), passa alla successiva e aggiungi a questa classe active (per visualizzarla)
     }
   })
+  // Analogamente, ma agendo sull'elemento del DOM di Classe 'prev', scorrerò le img in senso inverso
 
 });
