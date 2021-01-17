fac5cd19e9e4d597dc4ed52b7e1fdabfb2262a87
Vito Gentile <gentilevito1076@gmail.com>
2021-01-17 20:08:06 +0100

    Commenti

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
-$(document).ready(function() {
+$(document).ready(function() { //Esegui le istruzioni di codice che seguono solo dopo aver caricato il DOM. Questa istruzione, in realtà, non è necessaria visto che lo script.js è, nel file index, poco prima della chiusura del body e non nell'head.
+
+  $('.next').click(function() { //Al click sull'elemento di Classe 'next' verrà eseguito il blocco di codice tra Graffe
+
+  }
 
 });
